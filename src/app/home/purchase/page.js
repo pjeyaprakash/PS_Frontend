89e19849c1bbf5d39fca20420b67f24e611e9f6a
Jeyaprakash <p.jeyaprakashraja000@gmail.com>
2026-05-15 23:16:17 +0530

    PUSH

--- src/app/home/purchase/page.js
+++ src/app/home/purchase/page.js
@@ -4,14 +4,25 @@ import styles from './purchaseStyle.module.css'
 
 export default function Purchase() {
 
+    const a = [
+        {id:1, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:2, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:3, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:4, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:5, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:6, amount: 3000, date:"2026.12.11", type: "Dress"},
+        {id:7, amount: 3000, date:"2026.12.11", type: "Dress"},
+    ]
+
     return (
         <div className={styles.main}>
 
             {/* Dashboard */}
             <div className={styles.dashboard}>
+                <button>+ Create</button>
                 <div>Purchase Amount</div>
-                <div>N/A</div>
-                <div>N/A</div>
+                <div>Total</div>
+                <div>Amount</div>
             </div>
 
             {/* Middle */}
@@ -24,9 +35,30 @@ export default function Purchase() {
             <div className={styles.table}>
                 <table>
                     <thead>
-                        <td>kkk</td>
+                        <tr>
+                            <th>sn</th>
+                            <th>Amout</th>
+                            <th>Date</th>
+                            <th>Type</th> 
+                        </tr>
                     </thead>
                 </table>
+                <tbody>
+                    {a.length ?
+                        (a.map((row, i) => 
+                            <tr key = {i}>
+                                <td>row.id</td>
+                                <td>row.amount</td>
+                                <td>row.date</td>
+                                <td>row.type</td>
+                            </tr>
+                            )
+                        )
+                        :
+                        <div>No Purchase</div>
+                    }
+
+                </tbody>
             </div>
 
         </div>
