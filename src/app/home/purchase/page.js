1cfa9bad4457250f6847a3033657b754e1dba252
pjeyaprakash <p.jeyaprakashraja000@gmail.com>
2026-06-03 07:00:26 +0530

    PUSH

--- src/app/home/purchase/page.js
+++ src/app/home/purchase/page.js
@@ -5,16 +5,16 @@ import styles from './purchaseStyle.module.css'
 export default function Purchase() {
 
     const a = [
-        {id:1, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:2, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:3, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:4, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:5, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:6, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:7, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:8, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:9, amount: 3000, date:"2026.12.11", type: "Dress"},
-        {id:10, amount: 3000, date:"2026.12.11", type: "Dress"},
+        { id: 1, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 2, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 3, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 4, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 5, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 6, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 7, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 8, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 9, amount: 3000, date: "2026.12.11", type: "Dress" },
+        { id: 10, amount: 3000, date: "2026.12.11", type: "Dress" },
         // {id:11, amount: 3000, date:"2026.12.11", type: "Dress"},
         // {id:12, amount: 3000, date:"2026.12.11", type: "Dress"},
         // {id:13, amount: 3000, date:"2026.12.11", type: "Dress"},
@@ -45,38 +45,49 @@ export default function Purchase() {
 
                 {/* Middle */}
                 <div className={styles.middle}>
+
+                    <input className={styles.searchBox} type="search" placeholder="Search..." value={"search"}
+                        onChange={(e) => {
+                            // const value = e.target.value
+                            // setSearch(value)
+                            // if (debounceRef.current) clearTimeout(debounceRef.current);
+                            // debounceRef.current = setTimeout(() => {
+                            //     setSearchTerm(value); // API/search trigger
+                            // }, 1000);
+
+
+                        }} />
                     <div>Filter</div>
-                    <div>Search</div>
                     <button>+ Add Purchase</button>
                 </div>
 
                 <div>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <th>sn</th>
-                            <th>Amout</th>
-                            <th>Date</th>
-                            <th>Type</th> 
-                        </tr>
-                    </thead>       
-                    <tbody>
-                        {a.length ?
-                            (a.map((row, i) => 
-                                <tr key = {i}>
-                                    <td>{row.id}</td>
-                                    <td>{row.amount}</td>
-                                    <td>{row.date}</td>
-                                    <td>{row.type}</td>
-                                </tr>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>sn</th>
+                                <th>Amout</th>
+                                <th>Date</th>
+                                <th>Type</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {a.length ?
+                                (a.map((row, i) =>
+                                    <tr key={i}>
+                                        <td>{row.id}</td>
+                                        <td>{row.amount}</td>
+                                        <td>{row.date}</td>
+                                        <td>{row.type}</td>
+                                    </tr>
                                 )
-                            )
-                            :
-                            <div>No Purchase</div>
-                        }
-                    </tbody>
-                </table>
+                                )
+                                :
+                                <div>No Purchase</div>
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
